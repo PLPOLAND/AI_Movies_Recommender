1789b472014a14acc05e9c23751eb97850d70737
SwRafal <juniorekrafcio@interia.pl>
2019-12-10 17:09:54 +0100

    Podobni użytkownicy

--- src/main/java/AI/MoviesRecommender/Model/DataBase.java
+++ src/main/java/AI/MoviesRecommender/Model/DataBase.java
@@ -1,5 +1,6 @@
 package AI.MoviesRecommender.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +14,9 @@ import java.util.List;
      //Listy użytkowników i filmów
      List<User> users; 
      List<Film> films;
+
+     final float similarity_treshold = 0.75f;
+     final int similar_users_limit = 100;
 
      DataBase(List<User> users, List<Film> films)
      {
@@ -38,5 +42,43 @@ import java.util.List;
                 return i;
          }
          return null;
+     }
+
+     /* Algorytmy doboru */
+     public float getUsersSimilarity(Long first_ID, Long second_ID)
+     {
+         //...
+        return 0.0f;
+     }
+
+     public List<User> getSimilarUsers(Long ID)//Lista użytkowników o podobnych ocenach
+     { 
+        List<User> similar = new ArrayList<User>();
+        for(User i:this.users)
+        {
+            if(ID != i.getID())
+            {
+                if(getUsersSimilarity(ID, i.getID()) >= similarity_treshold)
+                    similar.add(i);
+            }
+        }
+
+        while(similar.size() > similar_users_limit)
+        {
+            float min_similarity = 1.f;
+            int index = 0;
+            for(User i:similar)
+            {
+                float user_similarity = getUsersSimilarity(ID, i.getID());
+                if(user_similarity < min_similarity)
+                {
+                    min_similarity = user_similarity;
+                    index = users.indexOf(i);
+                }
+            }
+            users.remove(index);
+        }
+
+        return similar;
      }
  }
