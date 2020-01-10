75455feb78cb14bda8dda68ac84c984e1aaf7ea0
SwRafal <juniorekrafcio@interia.pl>
2020-01-10 13:22:59 +0100

    Rejestracja kontynuacja

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -8,37 +8,50 @@ $(document).ready(function () {
         var useremail = $("#email").val();
         var userconfemail = $("#confemail").val();
 
-        if (nickname != "" && username != "" && userlastname != "" && password != "" && confpassword != "" && useremail != "" && userconfemail != "" && password == confpassword && useremail == userconfemail) {
+        if (nickname != "" && username != "" && userlastname != "" && password != "" && confpassword != "" && useremail != "" && userconfemail != "" &&
+            password == confpassword && useremail == userconfemail)
+        {
             $.ajax({
                 url: '/api/register',
                 type: 'get',
                 data: { nick: nickname, name: username, lastname: userlastname, pass: password, confpass: confpassword,
                         email: useremail, confemail: userconfemail },
-                success: function (response) {
+                success: function (response)
+                {
                     $("#err-msg").html(response);
                     
-                    var msg = "";
-                    if (response == 1) {
-                        window.location = "/";
-                    } else {
-                        msg = "Podano błędne dane"; 
-                        $("#err-msg").html(msg);
-                        $("#err-msg").show('slow');
+                    if (response == 1)
+                    {
+                        window.location = "/";  
                     }
-                    
                 }
             });
         }
         else
         {
+            var msg = "";
+                msg = "Pola nie mogą być puste"; 
+                $("#err-msg").html(msg);
+                
+
             if(password != confpassword)
+            {
                 $("#confpass").css("background-color", "#FF0000");
+                msg = "Hasło i jego potwierdzenie różnią się"; 
+                $("#err-msg").html(msg);
+            }
+     
             if (useremail != userconfemail)
+            {
                 $("#confemail").css("background-color", "#FF0000");
+                msg = "Email i jego potwierdzenie róźnią się"; 
+                $("#err-msg").html(msg);
+            }
+            $("#err-msg").show('slow');
         }
     });
     $(".container").click(function(){
 
-        $("#err-msg").hide('slow');
+        //$("#err-msg").hide('slow');
     })
 });
